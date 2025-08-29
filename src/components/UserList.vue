efeb4f329757ef4202b50d8043a549c894cc2fa5
christopherguala <guala.rojas.christopher@gmail.com>
2025-08-28 20:47:37 -0400

    se cambia js

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,23 +1,50 @@
 <script setup>
-    let usuarios = [];
+import { ref } from 'vue'
+    let usuarios = ref([]);
     class Usuario {
         constructor(id, nombre, edad, rut) {
             this.id = id;
             this.nombre = nombre;
             this.edad = edad;
             this.rut = rut;
-            usuarios.push(this);
         }
     }
 
-    new Usuario(1, "Juan", 20, "12.345.678-9");
-    new Usuario(2, "Pedro", 25, "23.456.789-0");
-    new Usuario(3, "Maria", 30, "34.567.890-1");
-    new Usuario(4, "Luis", 35, "45.678.901-2");
-    new Usuario(5, "Ana", 40, "56.789.012-3");
+    usuarios.value.push(
+    new Usuario(1, "Juan", 20, "12.345.678-9"),
+    new Usuario(2, "Pedro", 25, "23.456.789-0"),
+    new Usuario(3, "Maria", 30, "34.567.890-1"),
+    new Usuario(4, "Luis", 35, "45.678.901-2"),
+    new Usuario(5, "Ana", 40, "56.789.012-3")
+)
+
+    function crearUsuario() {
+    const nombre = prompt('Ingrese nombre:')
+    if (!nombre) return
+
+    const edad = prompt('Ingrese edad:')
+    if (!edad || isNaN(edad)) return alert('Edad no válida')
+
+    const rut = prompt('Ingrese RUT:')
+    if (!rut) return
+
+    const nuevoId =
+        usuarios.value.length > 0
+            ? Math.max(...usuarios.value.map((u) => u.id)) + 1
+            : 1
+
+    // Crear e insertar usuario
+    const nuevoUsuario = new Usuario(nuevoId, nombre, parseInt(edad), rut)
+    usuarios.value.push(nuevoUsuario)
+}
+
 </script>
 <template> 
+
     <div class="container mt-4">
+        <button class="btn btn-primary mb-3" @click="crearUsuario">
+      Agregar Usuario
+    </button>
     <table class="table table-dark table-striped table-bordered table-hover">
       <thead>
         <tr>
