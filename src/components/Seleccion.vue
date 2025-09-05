e2c2918130305cc2a2cd6c4eb4abb1684d637abc
christopherguala <guala.rojas.christopher@gmail.com>
2025-09-04 20:09:08 -0400

    se cambia letra, se poner rounded, etc

--- src/components/Seleccion.vue
+++ src/components/Seleccion.vue
@@ -1,12 +1,12 @@
 <template> <!--no entiendo por que me sale sugerencia amarilla en el < revisar luego xd  -->
   <div class="d-flex justify-content-center align-items-center min-vh-100">
     <div
-      class="p-4 text-white rounded shadow w-50 box-color"
+      class="p-4 text-white rounded shadow w-50 box-color rounded-4"
       style="min-width: 320px"
     >
       <h2 class="text-dark">Tipo de estudios</h2>
 
-      <div class="text-dark">
+      <div class="text-dark d-flex gap-3">
         <input
           type="radio"
           id="electricidad"
@@ -74,6 +74,9 @@ const diaSeleccionado = ref("");
 </script>
 
 <style scoped>
+* {
+  font-family: "Poppins", sans-serif;
+}
 .box-color {
   background-color: bisque;
   color: black;
